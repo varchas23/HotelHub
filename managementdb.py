0b4751fcd12f4e2447ac6c820aaddf0d76a826df
varchas23 <varchassharma23@gmail.com>
2024-04-03 18:29:57 -0400

    Pushes python file that views database to main

---
+++ managementdb.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+def condb():
+    # Connects database to python
+    con = sqlite3.connect("managementdb.db")
+    cur = con.cursor()
+
+    # Creates table for guests with guest information
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Guests (
+            GuestID INT PRIMARY KEY,
+            Name TEXT,
+            DOB DATE,
+            Address TEXT,
+            Email TEXT,
+            CreditCardInfo VARCHAR(19),
+            NumberOfGuests INT,
+            RoomNumber INT,
+            CheckInDate DATE,
+            CheckOutDate DATE,
+            SpecialRequests TEXT
+    );
+    """)
+
+    # Creates table for employees
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Employees (
+                EmployeeID INT PRIMARY KEY,
+                Name TEXT,
+                JoiningDate Date,
+                Username TEXT,
+                Password VARCHAR(20),
+                Email TEXT,
+                PhoneNumber VARCHAR(15),
+                Department TEXT,
+                EmergencyNumber VARCHAR(15)
+        );
+    """)
+
+    # Inserts employees to Employees table
+    cur.execute("""INSERT INTO 'Employees' ('Name', 'JoiningDate', 'Username', 'Password', 'Email', 'PhoneNumber', 'Department', 'EmergencyNumber') VALUES 
+                ('Larry O\'Brien', '2022-10-03', 'obrien3', 'treehouse1', 'larryobrien@gmail.com', '1234567890', 'Housekeeping', '2345678901');
+                """)
+
+    # Commits changes to database
+    con.commit()
+
+    # Closes the cursor and connection
+    cur.close()
+    con.close()
