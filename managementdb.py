470f000c39d34a41d5bc29e037c3eb3b7ae2e394
varchas23 <varchassharma23@gmail.com>
2024-04-03 14:25:55 -0400

    Created table for employees and added functionality to commit changes to managementdb.db and close the cursor.

--- managementdb.py
+++ managementdb.py
@@ -7,16 +7,38 @@ def condb():
 
     # Creates table for guests with guest information
     cur.execute("""
-    CREATE TABLE Guests (
-        GuestID INT PRIMARY KEY AUTO_INCREMENT,
-        Name TEXT,
-        DOB DATE,
-        Address VARCHAR(255)
-        CreditCardInfo VARCHAR(19)
-        NumberOfGuests INT
-        RoomNumber INT FOREIGN KEY REFERENCES (insert booking table)
-        CheckInDate DATE
-        CheckOutDate DATE
-        SpecialRequests TEXT
+        CREATE TABLE Guests (
+            GuestID INT PRIMARY KEY AUTO_INCREMENT,
+            Name TEXT,
+            DOB DATE,
+            Address VARCHAR(255),
+            Email VARCHAR(255)
+            CreditCardInfo VARCHAR(19),
+            NumberOfGuests INT,
+            RoomNumber INT FOREIGN KEY REFERENCES (insert booking table),
+            CheckInDate DATE,
+            CheckOutDate DATE,
+            SpecialRequests TEXT
     );
     """)
+
+    # Creates table for employees
+    cur.execute("""
+        CREATE TABLE Employees (
+                EmployeeID INT PRIMARY KEY AUTO_INCREMENT,
+                Name TEXT,
+                JoiningDate Date,
+                Username VARCHAR(255),
+                Password VARCHAR(20),
+                Email VARCHAR(255),
+                PhoneNumber VARCHAR(15),
+                Department TEXT,
+                EmergencyNumber VARCHAR(15)
+        );
+    """)
+
+    # Commits changes to database
+    con.commit()
+
+    # Closes the cursor
+    cur.close()
