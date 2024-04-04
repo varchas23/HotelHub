9b51b3ee4ef07c2a813d1d35f227a08b19f1dff7
Aleksandra Kalas <90855594+aleksandrakalas@users.noreply.github.com>
2024-04-03 22:18:21 -0400

    Adding connection from Login page with correct login to the home page
    Using controller to introduce connections between different frames

--- loginFrame.py
+++ loginFrame.py
@@ -12,7 +12,44 @@ from employeeInformation import EmployeeInformationWindow
 from employeeScheduleFrame import EmployeeScheduleWindow
 from homePageFrame import HomeWindow
 
+class LoginWindow(tk.Frame):
+        """
+        """
+        def __init__(self, parent, controller) -> None:
+            """
+            """
+            super().__init__(parent)
+            self.controller = controller
+            tk.Label(self, text="Login Window").pack(pady=10,padx=10)
+            # tk.Button(self, text="Home", command=lambda: controller.show_frame(HomeWindow)).pack()
+            tk.Label(self, text="LOGIN").pack(pady=10,padx=10)
+            tk.Label(self, text="Username:").pack()
+            self.username_entry = tk.Entry(self)
+            self.username_entry.pack()
 
+            tk.Label(self, text="Password:").pack()
+            self.password_entry = tk.Entry(self, show="*")
+            self.password_entry.pack()
+           
+            tk.Button(self, text="Login", command=self.login).pack()
+        
+        def login(self):
+            username = self.username_entry.get()
+            password = self.password_entry.get()
+        
+            if username == "manager" and password == "admin":
+                messagebox.showinfo("Login Success", "Welcome, Manager!")
+                # Make call for manager home page
+                #self.show_frame(HomeWindow)
+                self.controller.show_frame(HomeWindow)
+
+            elif username == "employee" and password == "1234":
+                messagebox.showinfo("Login Success", "Welcome, Employee!")
+                # Make call for employee home page
+                self.controller.show_frame(HomeWindow)
+
+            else:
+                messagebox.showerror("Login Failed", "Invalid credentials. Please try again.")  
 
 class HotelHub(tk.Tk):
     """
@@ -25,55 +62,36 @@ class HotelHub(tk.Tk):
         super().__init__(*args, **kwargs)
         self.title("HotelHub")
         self.geometry("1000x500")
-        
-        self.login_frame = tk.Frame(self)
-        self.login_frame.pack(padx=10, pady=10)
-        tk.Label(self.login_frame, text="LOGIN").grid(row=0, column=1)
-        tk.Label(self.login_frame, text="Username:").grid(row=1, column=0)
-        self.username_entry = tk.Entry(self.login_frame)
-        self.username_entry.grid(row=1, column=1)
-
-        tk.Label(self.login_frame, text="Password:").grid(row=2, column=0)
-        self.password_entry = tk.Entry(self.login_frame, show="*")
-        self.password_entry.grid(row=2, column=1)
-
-        tk.Button(self.login_frame, text="Login", command=self.login).grid(row=3, columnspan=2)
-
-        self.title("Hotel Management System")
+        self.frames = {}
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-        for F in (HomeWindow, BookingWindow):  # Add all frames
+        for F in (HomeWindow, BookingWindow, LoginWindow):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
+        
+        tk.Button(self, text="Login", command=self.show_frame(LoginWindow)).pack()
+        """tk.Label(self, text="LOGIN").pack(pady=10,padx=10)
+        tk.Label(self, text="Username:").pack()
+        self.username_entry = tk.Entry(self)
+        self.username_entry.pack()
+
+        tk.Label(self, text="Password:").pack()
+        self.password_entry = tk.Entry(self, show="*")
+        self.password_entry.pack()"""
 
         
-    
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
 
-    def login(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-        
-        if username == "manager" and password == "admin":
-            messagebox.showinfo("Login Success", "Welcome, Manager!")
-            # Make call for manager home page
-            self.show_frame(HomeWindow)
+    
+    
+    
 
-        elif username == "employee" and password == "1234":
-            messagebox.showinfo("Login Success", "Welcome, Employee!")
-            # Make call for employee home page
-            self.show_frame(HomeWindow)
-
-        else:
-            messagebox.showerror("Login Failed", "Invalid credentials. Please try again.")
 
 if __name__ == "__main__":
     app = HotelHub()
